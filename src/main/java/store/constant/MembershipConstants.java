b17dfe08631a41273879d183b5aab7c0a8a68b5d
Youn Sang Hyun <younsang523@gmail.com>
2024-11-09 03:13:53 +0900

    feat: 멤버쉽 할인률 상수화 구현

---
+++ src/main/java/store/constant/MembershipConstants.java
@@ -0,0 +1,6 @@
+package store.constant;
+
+public class MembershipConstants {
+    public static final int DISCOUNT_RATE = 30;
+    public static final int MAX_DISCOUNT_AMOUNT = 8000;
+}
---
+++ src/main/java/store/domain/product/Product.java
@@ -0,0 +1,12 @@
+package store.domain.product;
+
+public class Product {
+    private final String name;
+    private final int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+}
