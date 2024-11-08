54862f82e881594c28478e1a3aabce63a589cda9
Youn Sang Hyun <younsang523@gmail.com>
2024-11-09 03:17:56 +0900

    feat: 상품 정보 표시 기능 구현

--- src/main/java/store/domain/product/Product.java
+++ src/main/java/store/domain/product/Product.java
@@ -3,10 +3,22 @@ package store.domain.product;
 public class Product {
     private final String name;
     private final int price;
+    private final int quantity;
+    private final String promotionName;
 
-    public Product(String name, int price) {
+    public Product(String name, int price, int quantity, String promotionName) {
         this.name = name;
         this.price = price;
+        this.quantity = quantity;
+        this.promotionName = promotionName;
     }
 
+    public String getDisplayText() {
+        return String.format("- %s %,d원 %s%s",
+                name,
+                price,
+                quantity > 0 ? quantity + "개" : "재고 없음",
+                promotionName != null && !promotionName.equals("null") ? " " + promotionName : ""
+        );
+    }
 }
