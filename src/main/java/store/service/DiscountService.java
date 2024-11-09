dcacad8129c72cf48ab4a7783dbc9b00e55fa37f
Youn Sang Hyun <younsang523@gmail.com>
2024-11-09 23:54:20 +0900

    feat: 할인 계산 기능 구현

--- src/main/java/store/service/DiscountService.java
+++ src/main/java/store/service/DiscountService.java
@@ -1,4 +1,11 @@
 package store.service;
 
 public class DiscountService {
+    private static final int MEMBERSHIP_DISCOUNT_RATE = 30;
+    private static final int MAX_MEMBERSHIP_DISCOUNT = 8000;
+
+    public int calculateMembershipDiscount(int amount) {
+        int discountAmount = (amount * MEMBERSHIP_DISCOUNT_RATE) / 100;
+        return Math.min(discountAmount, MAX_MEMBERSHIP_DISCOUNT);
+    }
 }
